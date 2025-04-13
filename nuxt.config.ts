5c9c97be4b3166abbabde86443ea1b6f35cec729
Page Tailoring <bartek@pagetailoring.com>
2025-04-13 15:08:00 +0200

    move postcss.config to nuxt.config.ts & refactor, clean up formatting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,24 @@
-
 export default defineNuxtConfig({
   app: {
     head: {
       title: 'Vue - Momentum',
-      link: [
-        { rel: 'icon', type: 'image/png', href: '/favicon.png' }
-      ],
+      link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }],
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'description', content: 'Polish Football League standings and team information' }
-      ]
-    }
+        { name: 'description', content: 'Polish Football League standings and team information' },
+      ],
+    },
   },
 
   modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', '@nuxtjs/html-validator'],
 
   css: ['~/assets/main.css'],
-  
+  postcss: {
+    plugins: {
+      autoprefixer: {},
+    },
+  },
+
   devtools: { enabled: true },
   compatibilityDate: '2025-03-14',
 });
